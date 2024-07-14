c2f7309b32b207f8e8378de4bee30eabff9fb475
drkdvlpmnt <drkdvlpmnt@gmail.com>
2024-07-14 18:50:27 +0000

    Update soul.py

--- soul.py
+++ soul.py
@@ -84,7 +84,7 @@ async def start_asyncio_loop():
         await asyncio.sleep(REQUEST_INTERVAL)
 
 async def run_attack_command_async(target_ip, target_port, duration):
-    process = await asyncio.create_subprocess_shell(f"./bgmi {target_ip} {target_port} {duration} 500")
+    process = await asyncio.create_subprocess_shell(f"./soul {target_ip} {target_port} {duration} 500")
     await process.communicate()
 
 def is_user_admin(user_id, chat_id):
